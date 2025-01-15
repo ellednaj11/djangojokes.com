2d9f09ddc45773372855809e505bd5f9101ef27c
Jan Comilang <jandelle_comilang@emb.gov.ph>
2025-01-15 14:34:23 +0800

    with static

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     # Local apps
+    'common.apps.CommonConfig',
     'pages.apps.PagesConfig',
     'jokes.apps.JokesConfig',
+    
 
 ]
 
@@ -121,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+BASE_DIR / 'static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
